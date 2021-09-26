ed5eefe30dd384a574cfab3267925856998cb7a7
bnnk <50262917+bnnk@users.noreply.github.com>
2021-09-26 21:57:56 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
+console.log("Hello!");
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
